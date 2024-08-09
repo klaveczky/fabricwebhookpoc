d0246de1c5e51917835eabb4cd8e6d821a3d81db
Bence Márki <bence.marki@dakai.io>
2024-08-09 16:12:44 +0200

    feat: extend logging with headers

--- request-logger-middleware.js
+++ request-logger-middleware.js
@@ -2,9 +2,9 @@ module.exports = function requestLogger(req, _res, next) {
   console.info(
     `[${new Date().toISOString()}][REQUEST] ${req.method} ${
       req.url
-    } ${JSON.stringify(req.query)} from '${req.ip}' with body: ${JSON.stringify(
-      req.body
-    )}`
+    } ${JSON.stringify(req.query)} from '${req.ip}'\n
+    [HEADERS]: ${JSON.stringify(req.headers)}\n
+    [BODY]: ${JSON.stringify(req.body)}`
   )
   return next()
 }
